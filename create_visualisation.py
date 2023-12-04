a70bc1d6a16773b20ed8d2c119f5c6ef5b14ce6a
knolle1 <77113826+knolle1@users.noreply.github.com>
2023-12-04 22:14:06 +0000

    Add python and html files

---
+++ create_visualisation.py
@@ -0,0 +1,445 @@
+# -*- coding: utf-8 -*-
+"""
+Data for creating visualisation to investigate effects of 
+Covid-19 of public bike usage in Dublin.
+"""
+
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+# Default public token
+mapbox_access_token = "pk.eyJ1Ijoia25vbGxlMiIsImEiOiJjbHBsbzdxc2MwMXlrMmlwNjZncnFlNTYwIn0.oAe9EU2tXkl10y3UJn65xg"
+
+BLUE = '#636EFA'
+RED = '#EF553B'
+GREEN = '#00CC96'
+
+fig = make_subplots(
+    rows=3, cols=3,
+    specs=[[{'type': 'xy'}, {'type': 'mapbox'}, {'type': 'xy'}], 
+           [{'type': 'xy'}, {'type': 'mapbox'}, {'type': 'xy'}], 
+           [{'type': 'xy'}, {'type': 'mapbox'}, {'type': 'xy'}],],
+    subplot_titles=["<b>Average Daily Bike Usage and Weather</b>",
+                    "<b>Average Bike Usage Before/During/After Pandemic</b>",
+                    "<b>Seasonal Trends in Bike Usage</b>",
+                    None, None, None,
+                    None, None, None],
+    
+    horizontal_spacing=0.06,
+    vertical_spacing=0.05,
+    shared_xaxes=True,
+    )
+
+# Update xaxis properties
+fig.update_xaxes(title_text="Date", row=3, col=1, title_font=dict(size=12), title_standoff= 0)
+fig.update_xaxes(title_text="Time", row=3, col=3, title_font=dict(size=12), title_standoff= 0)
+
+# Update yaxis properties
+fig.update_yaxes(title_text="Daily Average<br>Bike Usage [%]", row=1, col=1, title_font=dict(size=12), title_standoff= 0)
+fig.update_yaxes(title_text="Temperature [°C]", row=2, col=1, title_font=dict(size=12), title_standoff= 0)
+fig.update_yaxes(title_text="Rainfall [mm]", row=3, col=1, title_font=dict(size=12), title_standoff= 0)
+
+fig.update_yaxes(title_text="Hourly/Daily<br>Average Bike Usage [%]", row=1, col=3, title_font=dict(size=12), title_standoff= 0)
+fig.update_yaxes(title_text="Hourly/Daily<br>Average Bike Usage [%]", row=2, col=3, title_font=dict(size=12), title_standoff= 0)
+fig.update_yaxes(title_text="Hourly/Daily<br>Average Bike Usage [%]", row=3, col=3, title_font=dict(size=12), title_standoff= 0)
+
+
+###############################################################################
+# Plot first column - bike usage and weather
+###############################################################################
+
+# Load dublinbikes data
+df_bikes = pd.read_csv("./data/dublinbikes_data_agg-date.csv")
+df_bikes['date'] = df_bikes['date'].astype('datetime64[ns]')
+df_bikes['pandemic'] = df_bikes['pandemic'].astype('string')
+df_bikes['average_bike_usage'] = df_bikes['average_bike_usage'].astype('float')
+
+
+#  Plot average bike usage over time
+# -----------------------------------
+
+fig.add_trace(go.Scatter(x=df_bikes[df_bikes["pandemic"] == "pre-pandemic"]["date"], 
+                         y=df_bikes[df_bikes["pandemic"] == "pre-pandemic"]["average_bike_usage"], 
+                         mode='lines', 
+                         line=dict(color = BLUE),
+                         name='Pre-pandemic',
+                         showlegend=True),
+              row=1, col=1)
+fig.add_trace(go.Scatter(x=df_bikes[df_bikes["pandemic"] == "pandemic"]["date"], 
+                         y=df_bikes[df_bikes["pandemic"] == "pandemic"]["average_bike_usage"], 
+                         mode='lines', 
+                         line=dict(color = RED),
+                         name='Pandemic',
+                         showlegend=True),
+              row=1, col=1)
+fig.add_trace(go.Scatter(x=df_bikes[df_bikes["pandemic"] == "post-pandemic"]["date"], 
+                         y=df_bikes[df_bikes["pandemic"] == "post-pandemic"]["average_bike_usage"], 
+                         mode='lines', 
+                         line=dict(color = GREEN),
+                         name='Post-pandemic',
+                         showlegend=True),
+              row=1, col=1)
+
+
+#  Plot temperature and rainfall data
+# ------------------------------------
+
+# Load weather data
+df_temp = pd.read_csv("./data/meteo_data.csv", skiprows=12)
+df_temp['date'] = df_temp['date'].astype('datetime64[ns]')
+df_temp['mint'] = pd.to_numeric(df_temp['mint'], errors='coerce')
+df_temp['maxt'] = pd.to_numeric(df_temp['maxt'], errors='coerce')
+df_temp = df_temp[(df_temp['date'] >= np.min(df_bikes['date'])) & (df_temp['date'] <= np.max(df_bikes['date']))]
+
+fig.add_trace(go.Scatter(x=df_temp["date"], 
+                         y=df_temp["mint"], 
+                         mode='lines', 
+                         line=dict(color = 'darkslateblue'),
+                         name='Min. temperature',
+                         showlegend=False),
+              row=2, col=1)
+
+fig.add_trace(go.Scatter(x=df_temp["date"], 
+                         y=df_temp["maxt"], 
+                         mode='lines', 
+                         line=dict(color = 'darkslateblue'),
+                         name='Max. temperature',
+                         fill='tonexty',
+                         showlegend=False),
+              row=2, col=1)
+
+fig.add_trace(go.Scatter(x=df_temp["date"], 
+                         y=df_temp["rain"], 
+                         mode='lines', 
+                         line=dict(color = 'darkslateblue'),
+                         name='Rainfall',
+                         showlegend=False),
+              row=3, col=1)
+
+###############################################################################
+# Plot second column - maps
+###############################################################################
+
+# Load dublinbikes data
+df_bikes = pd.read_csv("./data/dublinbikes_data_agg-station.csv")
+df_bikes['station'] = df_bikes['station'].astype('string')
+df_bikes['lat'] = df_bikes['lat'].astype('float')
+df_bikes['lon'] = df_bikes['lon'].astype('float')
+df_bikes['pandemic'] = df_bikes['pandemic'].astype('string')
+df_bikes['average_bike_usage'] = df_bikes['average_bike_usage'].astype('float')
+
+#  Plot average bike usage and geo data
+# -------------------------------------
+
+df = df_bikes[df_bikes["pandemic"] == "pre-pandemic"]
+fig.add_trace(go.Scattermapbox(lat = df['lat'],
+                               lon = df['lon'],
+                               mode = 'markers',
+                               marker = go.scattermapbox.Marker(color = BLUE,
+                                                                sizeref = 10,
+                                                                size = df['average_bike_usage']),
+                               text = (df['station'] + "<br>Average bike usage: " 
+                                       + df['average_bike_usage'].round(2).astype('string') + ' %'),
+                               name='Pre-pandemic',
+                               showlegend=False
+                              ),
+              row=1, col=2)
+
+df = df_bikes[df_bikes["pandemic"] == "pandemic"]
+fig.add_trace(go.Scattermapbox(lat = df['lat'],
+                               lon = df['lon'],
+                               mode = 'markers',
+                               marker = go.scattermapbox.Marker(color = RED,
+                                                                sizeref = 10,
+                                                                size = df['average_bike_usage']),
+                               text = (df['station'] + "<br>Average bike usage: " 
+                                       + df['average_bike_usage'].round(2).astype('string') + ' %'),
+                               name='Pandemic',
+                               showlegend=False
+                              ),
+              row=2, col=2)
+
+df = df_bikes[df_bikes["pandemic"] == "post-pandemic"]
+fig.add_trace(go.Scattermapbox(lat = df['lat'],
+                               lon = df['lon'],
+                               mode = 'markers',
+                               marker = go.scattermapbox.Marker(color = GREEN,
+                                                                sizeref = 10,
+                                                                size = df['average_bike_usage']),
+                               text = (df['station'] + "<br>Average bike usage: " 
+                                       + df['average_bike_usage'].round(2).astype('string') + ' %'),
+                               name='Post-pandemic',
+                               showlegend=False
+                              ),
+              row=3, col=2)
+
+
+###############################################################################
+# Plot third column - seasonal trends
+###############################################################################
+
+#  Plot data for daily trends
+# ---------------------------
+
+# Load dublinbikes data
+df_bikes = pd.read_csv("./data/dublinbikes_data_agg-hour.csv")
+df_bikes['hour'] = df_bikes['hour'].astype('datetime64[ns]')
+df_bikes['pandemic'] = df_bikes['pandemic'].astype('string')
+df_bikes['average_bike_usage'] = df_bikes['average_bike_usage'].astype('float')
+
+df = df_bikes[df_bikes["pandemic"] == "pre-pandemic"]
+fig.add_trace(go.Box(x=df["hour"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = BLUE),
+                     name='Pre-pandemic',
+                     showlegend=False, ),
+              row=1, col=3)
+
+df = df_bikes[df_bikes["pandemic"] == "pandemic"]
+fig.add_trace(go.Box(x=df["hour"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = RED),
+                     name='Pandemic',
+                     showlegend=False),
+              row=2, col=3)
+
+df = df_bikes[df_bikes["pandemic"] == "post-pandemic"]
+fig.add_trace(go.Box(x=df["hour"], 
+                     y=df["average_bike_usage"],
+                     #mode='markers', 
+                     line=dict(color = GREEN),
+                     name='Post-pandemic',
+                     showlegend=False),
+              row=3, col=3)
+
+fig.update_xaxes(tickformat="%H:%S", row=1, col=3)
+fig.update_xaxes(tickformat="%H:%S", row=2, col=3)
+fig.update_xaxes(tickformat="%H:%S", row=3, col=3)
+
+
+#  Plot data for weekly trends
+# ----------------------------
+
+# Load dublinbikes data
+df_bikes = pd.read_csv("./data/dublinbikes_data_agg-date.csv")
+df_bikes['day_of_week'] = df_bikes['day_of_week'].astype('datetime64[ns]')
+df_bikes['day'] = df_bikes['day'].astype('datetime64[ns]')
+df_bikes['month'] = df_bikes['month'].astype('datetime64[ns]')
+df_bikes['pandemic'] = df_bikes['pandemic'].astype('string')
+df_bikes['average_bike_usage'] = df_bikes['average_bike_usage'].astype('float')
+
+df = df_bikes[df_bikes["pandemic"] == "pre-pandemic"]
+fig.add_trace(go.Box(x=df["day_of_week"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = BLUE),
+                     name='Pre-pandemic',
+                     showlegend=False,
+                     visible=False),
+              row=1, col=3)
+
+df = df_bikes[df_bikes["pandemic"] == "pandemic"]
+fig.add_trace(go.Box(x=df["day_of_week"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = RED),
+                     name='Pandemic',
+                     showlegend=False,
+                     visible=False),
+              row=2, col=3)
+
+df = df_bikes[df_bikes["pandemic"] == "post-pandemic"]
+fig.add_trace(go.Box(x=df["day_of_week"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = GREEN),
+                     name='Post-pandemic',
+                     showlegend=False,
+                     visible=False),
+              row=3, col=3)
+
+
+#  Plot data for monthly trends
+# -----------------------------
+
+df = df_bikes[df_bikes["pandemic"] == "pre-pandemic"]
+fig.add_trace(go.Box(x=df["day"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = BLUE),
+                     name='Pre-pandemic',
+                     showlegend=False,
+                     visible=False),
+              row=1, col=3)
+
+df = df_bikes[df_bikes["pandemic"] == "pandemic"]
+fig.add_trace(go.Box(x=df["day"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = RED),
+                     name='Pandemic',
+                     showlegend=False,
+                     visible=False),
+              row=2, col=3)
+
+df = df_bikes[df_bikes["pandemic"] == "post-pandemic"]
+fig.add_trace(go.Box(x=df["day"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = GREEN),
+                     name='Post-pandemic',
+                     showlegend=False,
+                     visible=False),
+              row=3, col=3)
+
+
+#  Plot data for annual trends
+# ----------------------------
+
+df = df_bikes[df_bikes["pandemic"] == "pre-pandemic"]
+fig.add_trace(go.Box(x=df["month"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = BLUE),
+                     name='Pre-pandemic',
+                     showlegend=False, 
+                     visible=False),
+              row=1, col=3)
+
+df = df_bikes[df_bikes["pandemic"] == "pandemic"]
+fig.add_trace(go.Box(x=df["month"], 
+                     y=df["average_bike_usage"], 
+                     #mode='markers', 
+                     line=dict(color = RED),
+                     name='Pandemic',
+                     showlegend=False,
+                     visible=False),
+              row=2, col=3)
+
+df = df_bikes[df_bikes["pandemic"] == "post-pandemic"]
+fig.add_trace(go.Box(x=df["month"], 
+                     y=df["average_bike_usage"],
+                     #mode='markers', 
+                     line=dict(color = GREEN),
+                     name='Post-pandemic',
+                     showlegend=False,
+                     visible=False),
+              row=3, col=3)
+
+###############################################################################
+# Create dropdown menu
+###############################################################################
+
+updatemenus = list([
+    dict(active=0,
+         yanchor="top",
+         y=0.85,
+         xanchor='left',
+         x=1.01,
+         buttons=list([   
+            dict(label = 'Daily trends',
+                 method = 'update',
+                 args = [{'visible': [True]*9+[True]*3+[False]*3+[False]*3+[False]*3},
+                         {'xaxis2.tickformat':'%H:%S',
+                          'xaxis4.tickformat':'%H:%S',
+                          'xaxis6.tickformat':'%H:%S',
+                          'xaxis6.title':'Time'}
+                         ]),
+            dict(label = 'Weekly trends',
+                 method = 'update',
+                 args = [{'visible': [True]*9+[False]*3+[True]*3+[False]*3+[False]*3},
+                         {'xaxis2.tickformat':'%a',
+                          'xaxis4.tickformat':'%a',
+                          'xaxis6.tickformat':'%a',
+                          'xaxis6.title':'Day of the Week'}
+                         ]),
+            dict(label = 'Monthly trends',
+                 method = 'update',
+                 args = [{'visible': [True]*9+[False]*3+[False]*3+[True]*3+[False]*3},
+                         {'xaxis2.tickformat':'%d',
+                          'xaxis4.tickformat':'%d',
+                          'xaxis6.tickformat':'%d',
+                          'xaxis6.title':'Day of the Month'}
+                         ]),
+            dict(label = 'Annual trends',
+                 method = 'update',
+                 args = [{'visible': [True]*9+[False]*3+[False]*3+[False]*3+[True]*3},
+                         {'xaxis2.tickformat':'%b',
+                          'xaxis4.tickformat':'%b',
+                          'xaxis6.tickformat':'%b',
+                          'xaxis6.title':'Month'}
+                         ]),
+        ]),
+    )
+])
+
+###############################################################################
+# Update layouts
+###############################################################################
+
+fig.update_layout(title_text = 'Effects of Covid-19 on Public Bike Use in Dublin<br><span style="font-size: 12px">Bike usage defined as percentage of bike stands of a station that are available (i.e. not occupied by a bike).<br>Covid-19 pandemic defined as time period during which regulations and protective measures were in place (2020-03-12 to 2022-03-31).<br>Data sources: <a href="https://data.gov.ie/dataset/dublinbikes-api">data.gov.ie (Dublinbikes API)</a>, <a href="https://www.met.ie/climate/available-data/historical-data">www.met.ie</a> and <a href="https://www.citizensinformation.ie/en/health/covid19/public-health-measures-for-covid19/">www.citizensinformation.ie</a></span>',
+                  title_x=0.5,
+                  margin = dict(t = 150),
+                  title_y=0.95,
+                  #title_pad_b=64,
+                  hovermode='closest',
+                  
+                  legend=dict(yanchor="top",
+                              y=1,
+                              xanchor="left",
+                              x=1.01),
+                  
+                  updatemenus=updatemenus,
+                  
+                  mapbox=dict(accesstoken=mapbox_access_token,
+                              bearing=0,
+                              center=go.layout.mapbox.Center(lat=53.345,lon=-6.27),
+                              pitch=0,
+                              zoom=10.8
+                             ),
+                  mapbox2=dict(accesstoken=mapbox_access_token,
+                               bearing=0,
+                               center=go.layout.mapbox.Center(lat=53.345,lon=-6.27),
+                               pitch=0,
+                               zoom=10.8
+                               ),
+                  mapbox3=dict(accesstoken=mapbox_access_token,
+                               bearing=0,
+                               center=go.layout.mapbox.Center(lat=53.345,lon=-6.27),
+                               pitch=0,
+                               zoom=10.8
+                               ),
+                  height=750,
+                  
+                  yaxis = dict(tickfont = dict(size=10)),
+                  yaxis2 = dict(tickfont = dict(size=10)),
+                  yaxis3 = dict(tickfont = dict(size=10)),
+                  yaxis4 = dict(tickfont = dict(size=10)),
+                  yaxis5 = dict(tickfont = dict(size=10)),
+                  yaxis6 = dict(tickfont = dict(size=10)),
+                  
+                  xaxis = dict(tickfont = dict(size=10)),
+                  xaxis2 = dict(tickfont = dict(size=10)),
+                  xaxis3 = dict(tickfont = dict(size=10)),
+                  xaxis4 = dict(tickfont = dict(size=10)),
+                  xaxis5 = dict(tickfont = dict(size=10)),
+                  xaxis6 = dict(tickfont = dict(size=10)),
+                  
+                  xaxis_showticklabels=True, 
+                  xaxis2_showticklabels=True, 
+                  xaxis3_showticklabels=True, 
+                  xaxis4_showticklabels=True,  
+                  xaxis5_showticklabels=True,
+                  xaxis6_showticklabels=True,  
+
+                  )
+
+fig.update_annotations(font_size=12)
+
+#fig.show()
+fig.write_html("dublinbikes_covid19_visualisation.html")
---
+++ preprocess_data.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Code to join data from separate CSV files, pre-process and aggregate
+"""
+
+# Imports
+import pandas as pd
+import os
+from datetime import datetime
+import numpy as np
+
+
+# Script parameters
+# -----------------
+
+data_dir = "./data/dublinbikes_data"
+
+columns = ["TIME", "BIKE STANDS", "AVAILABLE BIKE STANDS", 
+           "STATUS", "ADDRESS", "LATITUDE", "LONGITUDE"]
+
+pandemic_start = datetime.strptime("2020-03-12 00:00:00", '%Y-%m-%d %H:%M:%S')
+pandemic_end = datetime.strptime("2022-03-31 23:59:59", '%Y-%m-%d %H:%M:%S')
+
+
+# Join dublinbikes data into single dataframe
+# -------------------------------------------
+
+df_bikes = pd.DataFrame(columns=["station", "lat", "lon", "bike_usage", "timestamp", "pandemic"])
+for filename in os.listdir(data_dir):
+    print(filename)
+    df = pd.read_csv(data_dir + "/" + filename)
+    
+    # Rename columns so that data directly downloadad from website and data collected using API are compatible
+    df = df.rename(columns={"harvest_time" : "TIME",
+                            "available_bike_stands" : "AVAILABLE BIKE STANDS",
+                            "bike_stands" : "BIKE STANDS",
+                            "status" : "STATUS",
+                            "address" : "ADDRESS",
+                            "latitude" : "LATITUDE",
+                            "longitude" : "LONGITUDE",})
+    
+    # Filter out closed stations
+    df = df[(df["STATUS"] == "Open") | (df["STATUS"] == "OPEN")]
+    
+    # Format station data
+    df["timestamp"] = df["TIME"].astype('datetime64[ns]')
+    df["station"] = df["ADDRESS"].astype("string")
+    df["lon"] = df["LONGITUDE"]
+    df["lat"] = df["LATITUDE"]
+    df["capacity"] = df["BIKE STANDS"]
+    df["bike_usage"] = df["AVAILABLE BIKE STANDS"] / df["BIKE STANDS"] * 100
+    
+    df["pandemic"] = np.where(df["timestamp"] < pandemic_start, 'pre-pandemic', 
+                              np.where(df["timestamp"] > pandemic_end, 'post-pandemic', 'pandemic'))
+    
+    # Select columns
+    df = df[["station", "lat", "lon", "bike_usage", "timestamp", "pandemic"]]
+    
+    df_bikes = pd.concat([df_bikes, df], ignore_index=True)
+
+# Drop any duplicates
+df_bikes = df_bikes.drop_duplicates(subset=['timestamp', 'station'])
+
+# Cast column types
+df_bikes['timestamp'] = df_bikes['timestamp'].astype('datetime64[ns]')
+df_bikes['station'] = df_bikes['station'].astype('string')
+df_bikes['pandemic'] = df_bikes['pandemic'].astype('string')
+df_bikes['date'] = df_bikes["timestamp"].dt.date.astype('datetime64[ns]')
+
+
+# Aggregate and save
+# ------------------
+
+# Aggregate by date for comparison with temperature data
+df_agg = df_bikes[["date", "pandemic", "bike_usage"]]
+df_agg = df_agg.groupby(["date", "pandemic"]).agg(['mean','sem']).reset_index()
+df_agg.columns = ["date", "pandemic", 'average_bike_usage','sem_bike_usage']
+df_agg["day_of_week"] = pd.to_datetime({'year':1970, 'month':1, 'day':5+df_agg['date'].dt.dayofweek}) # 01-01-1970 was a Thursday so the first Monday was on the 5th
+df_agg["day"] = pd.to_datetime({'year':1970, 'month':1, 'day':df_agg['date'].dt.day})
+df_agg["month"] = pd.to_datetime({'year':1970, 'month':df_agg['date'].dt.month, 'day':1})
+df_agg.to_csv("./data/dublinbikes_data_agg-date.csv")
+
+# Aggregate by station and time period to plot on map
+df_agg = df_bikes[["station", "lat", "lon", "pandemic", "bike_usage"]]
+df_agg = df_agg.groupby(["station", "lat", "lon", "pandemic"]).agg(['mean','sem']).reset_index()
+df_agg.columns = ["station", "lat", "lon", "pandemic", 'average_bike_usage','sem_bike_usage']
+df_agg.to_csv("./data/dublinbikes_data_agg-station.csv")
+
+# Aggregate by station and hour for daily trends
+df_agg = df_bikes[["date", "timestamp", "pandemic", "bike_usage"]]
+df_agg["hour"] = pd.to_datetime({'year':1970, 'month':1, 'day':1, 'hour':df_bikes['timestamp'].dt.hour})
+df_agg = df_agg.drop(columns=["timestamp"])
+df_agg = df_agg.groupby(["date", "hour", "pandemic"]).agg(['mean','sem']).reset_index()
+df_agg.columns = ["date", "hour", "pandemic", 'average_bike_usage','sem_bike_usage']
+df_agg.to_csv("./data/dublinbikes_data_agg-hour.csv")
